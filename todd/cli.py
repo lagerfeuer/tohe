ff5f03d0ab50bff85354494fc3255578cce7e3b9
Lukas Deutz <lukas.deutz@mailfence.com>
2020-02-15 12:53:48 +0100

    Fix minor issues with CLI and improve argument parsing

--- todd/cli.py
+++ todd/cli.py
@@ -10,6 +10,7 @@ from typing import Optional, List
 from shutil import get_terminal_size
 
 from todd.db import ToddDB
+from todd.util.editor import call_editor
 
 __VERSION__ = '0.1.0'
 
@@ -17,10 +18,11 @@ __VERSION__ = '0.1.0'
 def id_style(txt, rjust=0):
     if rjust > 0:
         txt = txt.rjust(rjust)
-    return Fore.BLUE + Style.BRIGHT + txt + Style.RESET_ALL
+    return Fore.BLUE + txt + '.' + Style.RESET_ALL
 
 
 def todo_style(txt, max_length):
+    txt = txt.split('\n')[0]
     if len(txt) >= max_length:
         txt = txt[:max_length - 4] + '...'
     return Fore.RED + Style.BRIGHT + txt + Style.RESET_ALL
@@ -44,24 +46,11 @@ def print_rows(rows):
         (id, todo, tags) = row
         id = id_style(str(id), rjust=longest_id)
         todo = todo_style(todo, term_width)
-        tags = tags_style(tags, rjust=longest_id)
+        tags = tags_style(tags, rjust=longest_id + 1)
 
         print(id, end=' ')
         print(todo, end=('' if todo.endswith('\n') else '\n'))
         print(tags)
-
-
-def call_editor(content: Optional[str] = None) -> str:
-    EDITOR = os.environ.get('EDITOR')
-    if EDITOR is None:
-        raise EnvironmentError('No $EDITOR specified!')
-    with tempfile.NamedTemporaryFile(prefix='todd_', suffix='.tmp') as tf:
-        if content:
-            tf.write(bytes(content, 'utf-8'))
-            tf.flush()
-        call([EDITOR, tf.name])
-        tf.seek(0)
-        return tf.read().decode('utf-8')
 
 
 def main(argv: List[str] = sys.argv) -> None:
@@ -71,16 +60,22 @@ def main(argv: List[str] = sys.argv) -> None:
         '-v', '--version', action='store_true', help='show version number and exit')
 
     ops = arg_parser.add_argument_group('OPERATIONS')
-    ops.add_argument('-a', '--add', nargs='?',
-                     metavar='CONTENT', help='add a new todo', const='_default', default=None)
-    ops.add_argument('-e', '--edit', help='edit a todo')
-    ops.add_argument('-l', '--list', help='add a new todo',
+    ops.add_argument('-a', '--add', nargs='?', metavar='CONTENT',
+                     help='add a new todo: if content is not supplied, $EDITOR will be opened',
+                     const='_default', default=None)
+    ops.add_argument('-l', '--list', help='list all todos',
                      action='store_true', default=False)
-    ops.add_argument('-s', '--search', help='search all todos')
-    ops.add_argument('-d', '--delete', help='delete todo')
+    ops.add_argument('-e', '--edit', help='edit a todo', metavar='ID')
+    ops.add_argument('-s', '--search', help='search all todos: TERM can be a phrase, word or tag',
+                     metavar='TERM')
+    ops.add_argument('-d', '--delete',
+                     help='delete todo by ID or tag', metavar='(ID|tag)')
 
     optops = arg_parser.add_argument_group('OPTIONAL')
-    optops.add_argument('-t', '--tag', help='list of tags: -t tag1,tag2')
+    optops.add_argument(
+        '-t', '--tag', help='add list of tags: -t tag1,tag2', metavar='TAGS')
+    optops.add_argument(
+        '-nt', '--notag', help='delete list of tags: -t tag1,tag2', metavar='TAGS')
 
     args = arg_parser.parse_args()
 
@@ -97,18 +92,27 @@ def main(argv: List[str] = sys.argv) -> None:
 
     if args.add:
         content = call_editor() if args.add == '_default' else args.add
-        if args.tag:
-            tags = args.tag.split(',')
         # FIXME: find better way to deal with tags
         # else:
         #     tags = call_editor(
         #         '# Enter tags below (one per line):').split('\n')
         #     tags = [t for t in tags if not t.startswith('#')]
+        tags = args.tag.split(',') if args.tag else []
         TODD.add(content, tags=tags)
 
-    if args.list:
+    elif args.list:
         rows = TODD.list()
         print_rows(rows)
+
+    elif args.edit:
+        pass
+    elif args.search:
+        pass
+    elif args.delete:
+        pass
+    else:
+        arg_parser.print_help()
+        sys.exit(1)
 
 
 if __name__ == "__main__":
